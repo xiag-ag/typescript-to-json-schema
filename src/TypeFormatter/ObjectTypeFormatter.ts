d6482a4b2a8db882727d9c0dbbd62ef303f57282
Alexandr Evtushenko <aevtushenko@xiag.ch>
2018-01-23 12:20:19 +0700

    Refactor `ObjectTypeFormatter`.

--- src/TypeFormatter/ObjectTypeFormatter.ts
+++ src/TypeFormatter/ObjectTypeFormatter.ts
@@ -28,8 +28,8 @@ export class ObjectTypeFormatter implements SubTypeFormatter {
         };
     }
     public getChildren(type: ObjectType): BaseType[] {
-        const properties: ObjectProperty[] = type.getProperties();
-        const additionalProperties: BaseType | boolean = type.getAdditionalProperties();
+        const properties = type.getProperties();
+        const additionalProperties = type.getAdditionalProperties();
 
         return [
             ...type.getBaseTypes().reduce((result: BaseType[], baseType: BaseType) => [
@@ -49,14 +49,13 @@ export class ObjectTypeFormatter implements SubTypeFormatter {
     }
 
     private getObjectDefinition(type: ObjectType): Definition {
-        const objectProperties: ObjectProperty[] = type.getProperties();
-        const additionalProperties: BaseType|boolean = type.getAdditionalProperties();
+        const objectProperties = type.getProperties();
+        const additionalProperties = type.getAdditionalProperties();
 
-        const required: string[] = objectProperties
+        const required = objectProperties
             .filter((property: ObjectProperty) => property.isRequired())
             .map((property: ObjectProperty) => property.getName());
-
-        const properties: StringMap<Definition> = objectProperties.reduce(
+        const properties = objectProperties.reduce(
             (result: StringMap<Definition>, property: ObjectProperty) => {
                 result[property.getName()] = this.childTypeFormatter.getDefinition(property.getType());
                 return result;
@@ -66,10 +65,16 @@ export class ObjectTypeFormatter implements SubTypeFormatter {
             type: "object",
             ...(Object.keys(properties).length > 0 ? {properties} : {}),
             ...(required.length > 0 ? {required} : {}),
-            ...(additionalProperties === true || additionalProperties instanceof AnyType ? {} :
-                {additionalProperties: additionalProperties instanceof BaseType ?
-                    this.childTypeFormatter.getDefinition(additionalProperties) :
-                    additionalProperties}),
+            ...this.getAdditionalProperties(additionalProperties),
         };
+    }
+    private getAdditionalProperties(additionalProperties: BaseType | boolean): Definition {
+        if (typeof additionalProperties === "boolean") {
+            return additionalProperties ? {} : {additionalProperties: false};
+        }
+
+        return additionalProperties instanceof AnyType
+            ? {}
+            : {additionalProperties: this.childTypeFormatter.getDefinition(additionalProperties)};
     }
 }

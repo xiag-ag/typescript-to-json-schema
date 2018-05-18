a0415e2090f0cf6bdfb7a23551891f53c2bb24bd
Alexandr Evtushenko <aevtushenko@xiag.ch>
2018-05-18 11:34:52 +0700

    Refactor package `types`.

---
+++ types/Errors.ts
@@ -0,0 +1,7 @@
+export * from "../src/Error/BaseError";
+export * from "../src/Error/LogicError";
+export * from "../src/Error/DiagnosticError";
+export * from "../src/Error/NoRootTypeError";
+export * from "../src/Error/UnknownNodeError";
+export * from "../src/Error/UnknownTypeError";
+export * from "../src/Error/AssertionError";
---
+++ types/Schema.ts
@@ -0,0 +1,3 @@
+export * from "../src/Schema/Definition";
+export * from "../src/Schema/Schema";
+export * from "../src/Schema/RawType";
